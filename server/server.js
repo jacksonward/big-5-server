9e119eaa3108e4e30595f80df0cc6149e3dd775a
jacksonward <37783252+jacksonward@users.noreply.github.com>
2018-07-05 14:01:01 -0500

    Add Support for all Traits
    Simplified calculation process, added support for all Big-5 Traits and removed some debugging features.

--- server/server.js
+++ server/server.js
@@ -5,16 +5,53 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.post("/testsubmit", function(req, res) {
+  var neuroticism = parseInt(0);
   var extroversion = parseInt(0);
+  var openness = parseInt(0);
+  var agreeableness = parseInt(0);
+  var conscientiousness = parseInt(0);
+
   console.log(req.body);
-  console.log('Answer to first question: "' + req.body.EXT1 + '".');
+  
+  //Neuroticism
   for (var key in req.body) {
-    if (key.includes("EXT")) {
-      console.log(req.body[key]);
+    if (key.includes("N")) {
+      neuroticism += parseInt(req.body[key]);
+    }
+  }
+
+  //Extroversion
+  for (var key in req.body) {
+    if (key.includes("E")) {
       extroversion += parseInt(req.body[key]);
     }
   }
+
+  //Openness
+  for (var key in req.body) {
+    if (key.includes("O")) {
+      openness += parseInt(req.body[key]);
+    }
+  }
+
+  //Agreeableness
+  for (var key in req.body) {
+    if (key.includes("A")) {
+      agreeableness += parseInt(req.body[key]);
+    }
+  }
+
+  //Conscientiousness
+  for (var key in req.body) {
+    if (key.includes("C")) {
+      conscientiousness += parseInt(req.body[key]);
+    }
+  }
+  console.log("neuroticism: " + neuroticism);
   console.log("extroversion: " + extroversion);
+  console.log("openness: " + openness);
+  console.log("agreeableness: " + agreeableness);
+  console.log("conscientiousness: " + conscientiousness);
 });
 
 app.listen(8080, function() {
