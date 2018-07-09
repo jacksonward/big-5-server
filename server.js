48736ec9d3ed093135f5d3e10471e6caf64eded5
jacksonward <37783252+jacksonward@users.noreply.github.com>
2018-07-09 14:37:02 -0500

    Huge Update
    Adds too much stuff for me to type here and I'm too excited to not just commit and go go go!

--- server.js
+++ server.js
@@ -3,27 +3,48 @@ var handlebars = require("handlebars");
 var inFile = "./hbs/report.hbs";
 var outFile = "./report.html";
 
+var ztable = require("ztable");
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+function roundToTwo(num) {    
+  return +(Math.round(num + "e+2")  + "e-2");
+}
+
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.use(express.static("./"))
+app.use(express.static("./"));
 
 //Trait Calculations
 app.post("/testsubmit", function(req, res) {
+  
+  //Initialize trait variables
   var neuroticism = parseInt(0);
   var extroversion = parseInt(0);
   var openness = parseInt(0);
   var agreeableness = parseInt(0);
   var conscientiousness = parseInt(0);
-
-  console.log(req.body);
   
+  //Keep track of #of questions answered for each trait so that we can average the trait later
+  var nq = parseInt(0);
+  var eq = parseInt(0);
+  var oq = parseInt(0);
+  var aq = parseInt(0);
+  var cq = parseInt(0);
+
+  //Initialize percentile variables to be calculated with ztable
+  npercentile = parseInt(0);
+  epercentile = parseInt(0);
+  opercentile = parseInt(0);
+  apercentile = parseInt(0);
+  cpercentile = parseInt(0);
+
   //Neuroticism
   for (var key in req.body) {
     if (key.includes("N")) {
+      nq ++;
       neuroticism += parseInt(req.body[key]);
     }
   }
@@ -31,6 +52,7 @@ app.post("/testsubmit", function(req, res) {
   //Extroversion
   for (var key in req.body) {
     if (key.includes("E")) {
+      eq ++;
       extroversion += parseInt(req.body[key]);
     }
   }
@@ -38,6 +60,7 @@ app.post("/testsubmit", function(req, res) {
   //Openness
   for (var key in req.body) {
     if (key.includes("O")) {
+      oq ++;
       openness += parseInt(req.body[key]);
     }
   }
@@ -45,6 +68,7 @@ app.post("/testsubmit", function(req, res) {
   //Agreeableness
   for (var key in req.body) {
     if (key.includes("A")) {
+      aq ++;
       agreeableness += parseInt(req.body[key]);
     }
   }
@@ -52,15 +76,48 @@ app.post("/testsubmit", function(req, res) {
   //Conscientiousness
   for (var key in req.body) {
     if (key.includes("C")) {
+      cq ++;
       conscientiousness += parseInt(req.body[key]);
     }
   }
+  
+  //Make sure nq..etc are not ZERO, which would make traits non-numerical
+  if (nq === 0) {
+    nq = 1;
+  }
+  if (eq === 0) {
+    eq = 1;
+  }
+  if (oq === 0) {
+    oq = 1;
+  }
+  if (aq === 0) {
+    aq = 1;
+  }
+  if (cq === 0) {
+    cq = 1;
+  }
+
+  //Calculate personal value here so it can be used in profile
+  neuroticism = (neuroticism / nq);
+  extroversion = (extroversion / eq);
+  openness = (openness / oq);
+  agreeableness = (agreeableness / aq);
+  conscientiousness = (conscientiousness / cq);
+
+  //Percentiles are calculated by subtracting trait mean from user value, 
+  //dividing that by trait std deviation, and then multiplying by 100
   var profile = {
     neuroticism: neuroticism,
     extroversion: extroversion,
     openness: openness,
     agreeableness: agreeableness,
-    conscientiousness: conscientiousness
+    conscientiousness: conscientiousness,
+    npercentile: (ztable((neuroticism - 3.095192454) / 0.6716282482) * 100),
+    epercentile: (ztable((extroversion - 3.076565749) / 0.3522392277) * 100),
+    opercentile: (ztable((openness - 3.313479385) / 0.384431613) * 100),
+    apercentile: (ztable((agreeableness - 3.204935388) / 0.3543837412) * 100),
+    cpercentile: (ztable((conscientiousness - 3.154749227) / 0.3885176637) * 100)
   };
 
   var source = fs.readFileSync(inFile, "utf8");
